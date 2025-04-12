0950a84e2d683530a562fd93acc9ccbe852e46f8
Aliaksei <Alex.Khmara2@gmail.com>
2025-04-12 10:42:20 +0300

    Обновил третью

--- lab3/main.py
+++ lab3/main.py
@@ -1,4 +1,3 @@
-# Основная программа
 from boolean_minimizer import BooleanMinimizer
 from parser import *
 from truth_table_generator import *
@@ -8,25 +7,28 @@ def main():
     tree = parser.parse()
     generator = TruthTableGenerator(tree, sorted(set(token for token in parser.tokens if token.isalnum())))    
     formulaSDNF, formulaSKNF = generator.generate() 
-    print(formulaSKNF)
-    form_type = int(input("Введите тип формы 1, если формула является СДНФ; 2, если СКНФ: "))
-    if form_type==1:
-        minimizer = BooleanMinimizer(formulaSDNF, form_type)
+    minimizerSDNF = BooleanMinimizer(formulaSDNF, 1)
+    minimizerSKNF = BooleanMinimizer(formulaSKNF, 2)
+    print("Расчётный:")
+    print("\n СДНФ:")
+    minimizerSDNF.minimize_calculative()
+    print("\n СКНФ:")
+    minimizerSKNF.minimize_calculative()
+    print("Расчётно-табличный:")
+    print("\n СДНФ:")
+    minimizerSDNF.minimize_calculative_tabular()
+    print("\n СКНФ:")
+    minimizerSKNF.minimize_calculative_tabular()  
+    print("Табличный")
+    if len(minimizerSDNF.variables)==5:
+        print("\n СДНФ:")
+        minimizerSDNF.minimize_karnaugh_table_5_var()
+        print("\n СКНФ:")
+        minimizerSKNF.minimize_karnaugh_table_5_var()
     else:
-        minimizer = BooleanMinimizer(formulaSKNF, form_type)
-    result = minimizer.minimize_karnaugh_table_5_var()
-    print(result)
-    print("\nВыберите метод минимизации:")
-    print("1. Расчетный метод")
-    print("2. Расчетно-табличный метод")
-    print("3. Табличный метод (Карта Карно)")
-    choice = input("Введите номер метода (1-3): ").strip()
-
-    methods = {'1': 'calculative', '2': 'calculative_tabular', '3': 'karnaugh'}
-    if choice in methods:
-        minimizer.run(methods[choice])
-    else:
-        print("Неверный выбор метода.")
-
+        print("\n СДНФ:")
+        minimizerSDNF.minimize_karnaugh()
+        print("\n СКНФ:")
+        minimizerSKNF.minimize_karnaugh()  
 if __name__ == "__main__":
     main()
